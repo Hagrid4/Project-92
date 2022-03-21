a1f5435366a8021d2c149c65c55a6defabb52f2e
Hagrid4 <88029651+Hagrid4@users.noreply.github.com>
2022-03-21 17:56:47 +0800

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,8 @@
+function addUsername()
+{
+    Player1_name = document.getElementById("Player1_name_input").value;
+    Player2_name = document.getElementById("Player2_name_input").value;
+    localStorage.setItem("Player1_name", Player1_name);
+    localStorage.setItem("Player2_name", Player2_name);
+    window.location.replace("quiz_game_page.html");
+}
